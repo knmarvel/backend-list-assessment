ff22e88f6da5f47a772dd0b9cd514ce018ed30b1
Karen Marvel <knmarvel@gmail.com>
2020-02-06 10:25:33 -0500

    list2 finished ready for grading

--- list2.py
+++ list2.py
@@ -18,17 +18,42 @@
 
 
 def remove_adjacent(nums):
-    """Your code goes here.  Edit this docstring."""
-    return
+    res = []
+    for el in nums:
+        if res:
+            if res[-1] != el:
+                res.append(el)
+        else:
+            res.append(el)
+    return res
 
 
 # E. Given two lists sorted in increasing order, create and return a merged
 # list of all the elements in sorted order. You may modify the passed in lists.
-# The solution should work in "linear" time, making a single pass of both lists.
+# The solution should work in "linear" time,making a single pass of both lists.
 # Hint: Don't use `sort` or `sorted` -- they are not linear time.
 def linear_merge(list1, list2):
-    """Your code goes here.  Edit this docstring."""
-    return
+    res = []
+    while len(list1) and len(list2):
+        if list1[0] < list2[0]:
+            el = list1[0]
+            list1 = list1[1:]
+        else:
+            el = list2[0]
+            list2 = list2[1:]
+        res.append(el)
+    while len(list1):
+        res.append(list1[0])
+        list1 = list1[1:]
+    while len(list2):
+        res.append(list2[0])
+        list2 = list2[1:]
+    return res
+# Kano's notes: looking for information I found
+# https://stackoverflow.com/questions/7237875/linear-merging-for-lists-in-python
+# which showed me how to use the while with two arguments.
+# I feel my end result looks way too much like the stackoverflow answer
+# I refactored as I could.
 
 
 # Simple provided test() function used in main() to print
